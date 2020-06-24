44fadfde668e3afc26f63d99141416d77077352b
rogue29 <rohitggulyani@gmail.com>
2020-06-24 11:57:42 +0530

    Initialized Spring Boot Project - (Web and Thymeleaf)

--- src/main/java/io/rogue/roguechucknorris/RogueChuckNorrisApplication.java
+++ src/main/java/io/rogue/roguechucknorris/RogueChuckNorrisApplication.java
@@ -1,13 +1,20 @@
 package io.rogue.roguechucknorris;
 
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RogueChuckNorrisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RogueChuckNorrisApplication.class, args);
+    }
+
+    @Bean
+    public ChuckNorrisQuotes chuckNorrisQuotes() {
+        return new ChuckNorrisQuotes();
     }
 
 }
